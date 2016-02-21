80fcfa2ffc95a2ca06894937006ea415b1087650
Aditya Sule <sule2@illinois.edu>
2016-02-21 01:32:06 -0600

    Made database changes for comments

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,5 @@
 class Place < ActiveRecord::Base
+	has_many :comments
 	def to_param
 		name
 	end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160221041709) do
+ActiveRecord::Schema.define(version: 20160221072834) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "place_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "comments", ["place_id"], name: "index_comments_on_place_id"
 
   create_table "places", force: :cascade do |t|
     t.string   "name"
